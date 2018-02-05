a9d2bf697f7144e2c9989ce38180f5d6b9814896
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2018-02-05 10:49:42 +0100

    Fixes R packages installation
    * Supress updates when installing msa
    * Adds symbolic links to libgfortran and libquadmath; the packages
    need them to compile.

--- src/main/scala/bundles/r.scala
+++ src/main/scala/bundles/r.scala
@@ -6,10 +6,19 @@ case object r extends Bundle() {
 
   def instructions: AnyInstructions =
     cmd("yum")("install", "-y", "R") -&-
+    // The packages below need to link libgfortran and libquadmath to compile
+    cmd("ln")("-s",
+      "/usr/lib64/libgfortran.so.3",
+      "/usr/lib/libgfortran.so"
+    ) -&-
+    cmd("ln")("-s",
+      "/usr/lib64/libquadmath.so.0",
+      "/usr/lib/libquadmath.so"
+    ) -&-
     cmd("R")("-e",
       """install.packages(c("ape", "seqinr"), repos="https://cloud.r-project.org/")
         |source("http://www.bioconductor.org/biocLite.R")
-        |biocLite("msa")
+        |biocLite("msa", suppressUpdates=TRUE)
         |library("msa")
         |library("ape")
         |library("seqinr")
