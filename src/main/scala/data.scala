da80f1b484cc302c1b441efdc1f780c7f2775549
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2019-03-04 16:01:34 +0100

    Add a referenceDB input in data

--- src/main/scala/data.scala
+++ src/main/scala/data.scala
@@ -10,55 +10,36 @@ case object data {
     case object r2 extends FileData(s"${prefix}.R2")(extension)
   }
 
-  /* ## 1. Raw reads QA, trimming and filtering */
+  /* ## 1. UMI clustering and MIGs size filtering */
 
-  /* ### Input reads */
-  // case object demultiplexed extends R1R2("demultiplexed")("fastq.gz")
+  /* ### Input */
+
+  // Reads
   case object r1 extends Data("r1")
   case object r2 extends Data("r2")
 
   /* ### Output */
-
-  case object qaReport {
-    /* - **1.1** Raw reads QA files */
-    case object raw     extends R1R2("qa_report_raw")("html")
-    /* - **1.3** QA file of preprocessed reads */
-    case object trimmed extends R1R2("qa_report_trimmed")("html")
-  }
-  /* - **1.2** FASTQ files with preprocessed reads */
-  case object trimmed {
-    case object   paired extends R1R2("trimmed_paired")("fastq.gz")
-    case object unpaired extends R1R2("trimmed_unpaired")("fastq.gz")
-  }
-
-  /* ## 2. UMI clustering and MIGs size filtering */
-
-  /* ### Input
-
-     - output **1.2**: `trimmedReads`
-  */
-
-  /* ### Output */
   case object mig {
 
-    /* - **2.1** MIG sequences */
+    /* - **1.1** MIG sequences */
     case object clusters extends Data("clusters")
     case object errors   extends Data("errors")
 
     case object size {
 
-      /* - **2.2** MIGs size report */
+      /* - **1.2** MIGs size report */
       case object report    extends FileData("mig_size")("tsv")
-      /* - **2.3** MIGs histogram */
+      /* - **1.3** MIGs histogram */
       case object histogram extends FileData("mig_size_histogram")("tsv")
     }
 
-    // This will be generated from the output of clusters (one task for each cluster)
-    case object cluster  extends R1R2("MIG")("fastq.gz")
+    // This will be generated from the output of clusters (one task for each
+    // cluster)
+    case object cluster extends R1R2("MIG")("fastq.gz")
   }
 
 
-  /* ## 5. MIGs assembly */
+  /* ## 2. MIGs assembly */
 
   /* ### Input
 
@@ -67,7 +48,7 @@ case object data {
 
   /* ### Output */
 
-  /* - **5.1** MIG Consensus sequence */
+  /* - **2.1** MIG Consensus sequence */
   case object consensus {
 
     // A zipped folder with all consensus pairs one FASTQ file per pair:
@@ -81,52 +62,58 @@ case object data {
     case object fastq extends FileData("consensus")("fastq")
   }
 
-  /* - **5.2** MIG SPAdes contigs */
+  /* - **2.2** MIG SPAdes contigs */
   case object scaffolds extends FileData("scaffolds")("fasta")
 
 
-  /* ## 6. Annotation and clonotype definition and clustering */
-  /* ## 7. Clonotype counting */
+  /* ## 3. Annotation and clonotype definition, clustering and
+           clonotype counting */
 
   /* ### Input
 
      - MIGs consensus sequences (output **5.1**)
+     - Reference database: {human,mouse}-TCR-{alpha,beta}
   */
+  case object referenceDB extends Data("db")
 
   /* ### Output */
   case object clonotype {
     case object totals extends FileData("clonotypes_totals")("tsv")
 
-    /* - **6.1** CDR3 clonotype nucleotide sequence */
+    /* - **3.1** CDR3 clonotype nucleotide sequence */
     case object CDR3 extends FileData("clonotype_cdr3")("fasta")
 
-    /* - **6.2** IgBLAST results */
+    /* - **3.2** IgBLAST results */
     case object igblastOut extends FileData("clonotypes_out")("txt")
     case object igblastTSV extends FileData("clonotypes_all")("tsv")
-    case object igblastProductiveTSV  extends FileData("clonotype_productive")("tsv")
-    case object igblastProductiveJSON extends FileData("clonotype_productive")("json")
+    case object igblastProductiveTSV  extends
+      FileData("clonotype_productive")("tsv")
+    case object igblastProductiveJSON extends
+      FileData("clonotype_productive")("json")
 
-    /* - **6.3** Clonotype-MIG table */
+    /* - **3.3** Clonotype-MIG table */
     case object migTable extends FileData("clonotype_mig")("tsv")
 
-    /* - **6.4** Clonotype table */
+    /* - **3.4** Clonotype table */
     case object infoTable extends FileData("clonotype_info")("tsv")
 
-    /* - **7.1** Clonotype counts table */
-    // **: This is an extended version of the clonotype table (output **6.4**) which also contains the counts and frequency of each clonotype. It is a TSV table with the following structure:
+    /* - **3.1** Clonotype counts table */
+    // **: This is an extended version of the clonotype table (output **3.4**)
+    // which also contains the counts and frequency of each clonotype. It is a
+    // TSV table with the following structure:
     case object countsTable extends FileData("clonotype_counts")("tsv")
   }
 
-  /* ## 8. Visualizations */
+  /* ## 5. Visualizations */
 
   /* ### Input
 
-     - IgBLAST Productive Clonotypes TSV (output **6.2.3**)
+     - IgBLAST Productive Clonotypes TSV (output **3.2.3**)
   */
 
   /* ### Output */
   case object viz {
-    /* - **8.1** Phylogenetic tree */
+    /* - **5.1** Phylogenetic tree */
     case object phylogeneticTree extends FileData("phylogenetic_tree")("png")
   }
 }
